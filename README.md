e31cbbcc8cd2f82f1ce5a992e844e65ebc7b47e2
dojomi <topfenmaler@gmail.com>
2015-12-04 14:09:57 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+	
+	#installation
+	bash <(curl -L https://raw.githubusercontent.com/DoJoMi/dotbash/master/setup.sh) -i
+	
+	#restore to old settings
+	bash <(curl -L https://raw.githubusercontent.com/DoJoMi/dotbash/master/setup.sh) -r
+	
--- setup.sh
+++ setup.sh
@@ -18,32 +18,23 @@ function pause(){
    read -p "$*"
 }
 
-show_help(){
-	echo "Usage:" $0  " [-i] [-r] [-h] "
-	echo "-h	help"
-	echo "-i	install"
-	echo "-r	remove all"
-	echo ""
-	exit 0	
-}
-
 install() {
 	if [ -x /usr/bin/git ]; 
-		then 
-			echo ""
-			color " --> Git installed, perfect..."
-			pause 'Press [Enter] key to continue...'
-			color " --> Installing .bash.... "
-			rm -rf $HOME/.bash_files
-			git clone https://github.com/DoJoMi/dotbash.git $HOME/.bash
-			ln -s $HOME/.bash/.bashrc $HOME/.bashrc
-			rm -rf $HOME/.bash/.git
-			color " --> Everthing installed.... "
-			pause 'Press [Enter] key to continue...'
-			echo "***************************************"
-			echo "Enter 'source .bashrc' to reload file"
-			echo "and 'show' to see more information"
-			echo "***************************************"
+	then 
+		echo ""
+		color " --> Git installed, perfect..."
+		pause 'Press [Enter] key to continue...'
+		color " --> Installing .bash.... "
+		rm -rf $HOME/.bash_files
+		git clone https://github.com/DoJoMi/dotbash.git $HOME/.bash
+		ln -s $HOME/.bash/.bashrc $HOME/.bashrc
+		rm -rf $HOME/.bash/.git
+		color " --> Everthing installed.... "
+		pause 'Press [Enter] key to continue...'
+		echo "***************************************"
+		echo "Enter 'source .bashrc' to reload file"
+		echo "and 'show' to see more information"
+		echo "***************************************"
 		else "oO git is not installed... just do it first ;)";
 	fi
 }
@@ -63,25 +54,15 @@ remove(){
 	color "Thank you for using....Have a nice day =)"
 }
 
-
-if [ $# -ne 1 ]; 
-then
-	show_help
-fi
-
-while getopts "irh" opt; do
+while getopts "ir" opt; do
 	case $opt in
-		
-		i)
-			logo
-			backup
-			install
-			;;
-		r)
-			remove
-			;;
-		h)
-			show_help
-			;;
+	i)
+		logo
+		backup
+		install
+		;;
+	r)
+		remove
+		;;
 	esac
 done
